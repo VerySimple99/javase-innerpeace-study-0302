928a82ac149cf17690b142277bc6dc573810d2ee
VerySimple99 <lyricrain.seo@gmail.com>
2025-03-02 21:10:43 +0900

    feat : 하하하

--- letgo-app/src/awareness/HelloAwareness.java
+++ letgo-app/src/awareness/HelloAwareness.java
@@ -2,6 +2,7 @@ package awareness;
 
 public class HelloAwareness {
 	public static void main(String[] args) {
-		System.out.println("Hello Awareness");
+		System.out.println("Hello Awareness:)");
+		System.out.println("하하하");
 	}
 }
