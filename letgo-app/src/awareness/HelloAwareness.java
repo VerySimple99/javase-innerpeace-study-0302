76338fb5bfd0b679faed33bac990d0c62567e3dc
VerySimple99 <lyricrain.seo@gmail.com>
2025-03-03 12:33:32 +0900

    feat: car3

--- letgo-app/src/awareness/HelloAwareness.java
+++ letgo-app/src/awareness/HelloAwareness.java
@@ -6,5 +6,7 @@ public class HelloAwareness {
 		System.out.println("main");
 		System.out.println("Hello Awareness:)");
 		System.out.println("하하하");
+		System.out.println("car2");
+		System.out.println("car3");
 	}
 }
